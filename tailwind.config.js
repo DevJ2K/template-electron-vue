f99c4153c572ef9034e369d6273d674b7255ea92
J2K <116686309+DevJ2K@users.noreply.github.com>
2025-01-05 17:02:15 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    ".src/renderer/index.html",
+    "./src/renderer/index.html",
     "./src/renderer/src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   darkMode: "class",
