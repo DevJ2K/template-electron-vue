e79e197ae73e5a48ce871169bb51afb77bc99ff0
DevJ2K <ajavontheopro@gmail.com>
2025-01-05 04:45:09 +0100

    ✅ : Add files...

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import Versions from './components/Versions.vue'
+
+
+window.electron.ipcRenderer.on('pong', () => {
+  console.log("Receive pong from backend...")
+})
+
+const ipcHandle = () => {
+  console.log("UI sending ping...")
+  window.electron.ipcRenderer.send('ping')
+}
+// window.addEventListener('resize', () => {
+//     console.log(`Window width: ${window.innerWidth}, Window height: ${window.innerHeight}`);
+// });
+</script>
+
+<template>
+  <img alt="logo" class="logo" src="./assets/electron.svg" />
+  <div class="creator">Powered by electron-vite</div>
+  <div class="text">
+    Build an Electron app with
+    <span class="vue">Vue</span>
+    and
+    <span class="ts">TypeScript</span>
+  </div>
+  <h1 class="font-bold">Tailwind is <span class="font-bold text-green-500">working</span> !</h1>
+  <p class="tip">Please try pressing <code>F12</code> to open the devTool</p>
+  <div class="actions">
+    <div class="action">
+      <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">Documentation</a>
+    </div>
+    <div class="action">
+      <a target="_blank" rel="noreferrer" @click="ipcHandle">Send IPC</a>
+    </div>
+  </div>
+  <Versions />
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    ".src/renderer/index.html",
+    "./src/renderer/src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
+  theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      // b_project_card: "930px"
+    },
+    extend: {
+      colors: {
+        "app-bg": "#FDFDFE", // 1. App background
+        "subtle-bg": "#F7F9FF", // 2. Subtle background
+        "ui-bg": "#EDF2FE", // 3. UI element background
+        "hover-ui-bg": "#E1E9FF", // 4. Hovered UI element background
+        "active-ui-bg": "#D2DEFF", // 5. Active / Selected UI element background
+        "subtle-border": "#C1D0FF", // 6. Subtle borders and separators
+        "ui-border": "#ABBDF9", // 7. UI element border and focus ring
+        "hover-ui-border": "#8DA4EF", // 8. Hovered UI element border
+        "accent-color": "#3E63DD", // 9. Solid backgrounds
+        "hover-accent-color": "#3358D4", // 10. Hovered solid backgrounds
+        "low-contrast-text": "#3A5BC7", // 11. Low-contrast text
+        "high-contrast-text": "#1F2D5C", // 12. High-contrast text
+      }
+    },
+    fontFamily: {
+      Poppins: ["Poppins, sans-serif"],
+    },
+    container: {
+      marginLeft: "auto",
+      marginRight: "auto",
+      width: "100%",
+      padding: "2rem",
+      center: true,
+      screens: {
+        'sm': '100%',
+        'md': '100%',
+        'lg': '1100px', // 48rem
+        'xl': '1100px'
+      }
+    }
+  },
+  plugins: [],
+}
+
